4c5271dd581bf1d4d1d59e06e86d66083a63ab2d
unknown <dakeke777@gmail.com>
2020-10-06 01:01:48 -0400

    hello golang

---
+++ src/go_code/project01/main/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello, world")
+}
+
+// compile the code by "go build hello.go"
+// execute by "hello.exe"
+
+// directly run "go run hello.go"
